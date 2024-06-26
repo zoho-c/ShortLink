51b403b40d4476b3eee7c16b57be4d5a6e68ef03
Hao Zhou <57220730+zoho-c@users.noreply.github.com>
2024-06-26 20:35:07 +0800

    feature: 用户手机号脱敏

---
+++ admin/src/main/java/cn/zhouhao/shortlink/admin/common/serialize/PhoneDesensitizationSerializer.java
@@ -0,0 +1,22 @@
+package cn.zhouhao.shortlink.admin.common.serialize;
+
+import cn.hutool.core.util.DesensitizedUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @author hiroshi
+ * @version 1.0
+ */
+public class PhoneDesensitizationSerializer extends JsonSerializer<String> {
+
+
+    @Override
+    public void serialize(String phone, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String phoneDesensitization = DesensitizedUtil.mobilePhone(phone);
+        jsonGenerator.writeString(phoneDesensitization);
+    }
+}
