037be4181f2ec89cbb5764475320f2e8bc45b5fa
Hao Zhou <57220730+zoho-c@users.noreply.github.com>
2024-06-26 20:34:45 +0800

    feature: 错误码

---
+++ admin/src/main/java/cn/zhouhao/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,18 @@
+package cn.zhouhao.shortlink.admin.common.convention.errorcode;
+
+/**
+ * @author hiroshi
+ * @version 1.0
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
