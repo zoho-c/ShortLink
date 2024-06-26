80d39f328b95c5aa296d2aa386ef2ae09cccf7cc
Hao Zhou <57220730+zoho-c@users.noreply.github.com>
2024-06-26 20:34:22 +0800

    feature: 用户请求以及响应传输对象

---
+++ admin/src/main/java/cn/zhouhao/shortlink/admin/dto/req/UserReqDTO.java
@@ -0,0 +1,8 @@
+package cn.zhouhao.shortlink.admin.dto.req;
+
+/**
+ * @author hiroshi
+ * @version 1.0
+ */
+public class UserReqDTO {
+}
---
+++ admin/src/main/java/cn/zhouhao/shortlink/admin/dto/resp/UserRespDTO.java
@@ -0,0 +1,40 @@
+package cn.zhouhao.shortlink.admin.dto.resp;
+
+import cn.zhouhao.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+
+/**
+ * 用户返回实体参数响应
+ *
+ * @author hiroshi
+ * @version 1.0
+ */
+@Data
+public class UserRespDTO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
