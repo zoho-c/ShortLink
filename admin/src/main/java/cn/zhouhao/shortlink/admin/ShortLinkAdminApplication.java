eeb9e7ae33e39449f3ac6b6f195db181be2d4a43
Hao Zhou <57220730+zoho-c@users.noreply.github.com>
2024-06-26 20:32:54 +0800

    feature: SpringBoot启动类

---
+++ admin/src/main/java/cn/zhouhao/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package cn.zhouhao.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hiroshi
+ * @version 1.0
+ */
+@SpringBootApplication
+@MapperScan("cn.zhouhao.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
